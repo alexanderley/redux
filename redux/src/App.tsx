911b1197065d52086a60fbe539fca9acc65e15c4
alexanderley <112874444+alexanderley@users.noreply.github.com>
2025-08-14 19:58:20 +0200

    added new redux slice

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, incrementByAmount } from "./store/counterSlice";
+import { toggleTheme } from "./store/themeSlice";
 
 function App() {
   const count = useSelector((state: any) => state.counter.value);
+  const theme = useSelector((state: any) => state.theme.theme);
   const dispatch = useDispatch();
 
   return (
     <>
       <h1>Counter: {count}</h1>
+      <h1>Theme: {theme}</h1>
+      <button onClick={() => dispatch(toggleTheme())}>Toggle Theme</button>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(incrementByAmount(5))}>
